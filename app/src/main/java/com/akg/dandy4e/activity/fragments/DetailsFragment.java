a3356a8a0810953bf0f5f57df99b0043bd113653
Adrien Gervais <adrien.k.gervais@gmail.com>
2016-02-16 16:53:34 -0500

    Added fragment details class

---
+++ app/src/main/java/com/akg/dandy4e/activity/fragments/DetailsFragment.java
@@ -0,0 +1,16 @@
+package com.akg.dandy4e.activity.fragments;
+import android.app.*;
+import android.view.*;
+import android.os.*;
+import com.akg.dandy4e.*;
+
+public class DetailsFragment extends Fragment
+{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		return inflater.inflate(R.layout.fragment_details, container, false);
+	}
+}
